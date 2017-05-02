b3c72eb8de5d92d2ff4bb42ea2c40acdfc7f7a57
tonypig93 <tony98370@qq.com>
2017-05-02 09:24:12 +0800

    new working from mac

--- frontend/src/systemjs.config.js
+++ frontend/src/systemjs.config.js
@@ -26,7 +26,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'ng2-bootstrap': 'npm:ng2-bootstrap/bundles/ngx-bootstrap.umd.min.js'
+      'ngx-bootstrap': 'npm:ngx-bootstrap/bundles/ngx-bootstrap.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
