e06e12026a54d98fb235789e363b1fab47fab013
tonypig93 <tony98370@qq.com>
2017-05-04 17:09:17 +0800

    sql script

---
+++ dump.sql
@@ -0,0 +1,171 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: vcdb
+-- ------------------------------------------------------
+-- Server version	5.7.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `GROUPS`
+--
+
+DROP TABLE IF EXISTS `GROUPS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `GROUPS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `GROUP_NAME` varchar(45) NOT NULL,
+  `DESCRIPTION` varchar(1000) DEFAULT NULL,
+  `PARENT_ID` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  UNIQUE KEY `GroupName_UNIQUE` (`GROUP_NAME`)
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `GROUPS`
+--
+
+LOCK TABLES `GROUPS` WRITE;
+/*!40000 ALTER TABLE `GROUPS` DISABLE KEYS */;
+INSERT INTO `GROUPS` VALUES (1000,'YTWO','a company',0),(1001,'ZTWO','hehe',0),(1002,'XTWO','destroy the world with codes',0);
+/*!40000 ALTER TABLE `GROUPS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PRJ_MEMBER`
+--
+
+DROP TABLE IF EXISTS `PRJ_MEMBER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PRJ_MEMBER` (
+  `ID` int(11) NOT NULL DEFAULT '1000',
+  `USER_FK` int(11) NOT NULL,
+  `PRJ_FK` int(11) NOT NULL,
+  `INPUT_DATE` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PRJ_MEMBER`
+--
+
+LOCK TABLES `PRJ_MEMBER` WRITE;
+/*!40000 ALTER TABLE `PRJ_MEMBER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PRJ_MEMBER` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PRJ_VERSION`
+--
+
+DROP TABLE IF EXISTS `PRJ_VERSION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PRJ_VERSION` (
+  `ID` int(11) NOT NULL DEFAULT '1000',
+  `PRJ_FK` int(11) NOT NULL,
+  `V_MAJOR` int(11) NOT NULL,
+  `V_MINOR` int(11) NOT NULL,
+  `V_PATCH` int(11) NOT NULL,
+  `USER_FK` int(11) NOT NULL,
+  `INPUT_DATE` varchar(45) DEFAULT 'now()',
+  `REPO_CODE` varchar(45) DEFAULT NULL,
+  `LOG_BUG` varchar(2000) DEFAULT NULL,
+  `LOG_FEATURE` varchar(2000) DEFAULT NULL,
+  `LOG_GENERAL` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PRJ_VERSION`
+--
+
+LOCK TABLES `PRJ_VERSION` WRITE;
+/*!40000 ALTER TABLE `PRJ_VERSION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PRJ_VERSION` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PROJECT`
+--
+
+DROP TABLE IF EXISTS `PROJECT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PROJECT` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PRJ_NAME` varchar(45) NOT NULL,
+  `GROUP_FK` int(11) NOT NULL DEFAULT '0',
+  `IS_PUBLIC` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  UNIQUE KEY `PRJ_NAME_UNIQUE` (`PRJ_NAME`)
+) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PROJECT`
+--
+
+LOCK TABLES `PROJECT` WRITE;
+/*!40000 ALTER TABLE `PROJECT` DISABLE KEYS */;
+INSERT INTO `PROJECT` VALUES (1000,'YTWO App',1000,1),(1003,'YTWO desktop',1000,1);
+/*!40000 ALTER TABLE `PROJECT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `USER_NAME` varchar(45) NOT NULL,
+  `PASSWORD` varchar(45) NOT NULL,
+  `INPUT_DATE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `GROUP_FK` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  KEY `GROUP_FK_idx` (`GROUP_FK`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER`
+--
+
+LOCK TABLES `USER` WRITE;
+/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
+INSERT INTO `USER` VALUES (1000,'hay','202cb962ac59075b964b07152d234b70','2017-05-03 09:59:23',1000),(1001,'tony','123','2017-05-03 10:00:18',1000);
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-04 17:06:31
