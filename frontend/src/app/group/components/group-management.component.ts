7eb264e6934dcc0abe824236c2c215bd74e0ba59
tonypig93 <tony98370@qq.com>
2017-05-04 22:52:34 +0800

    group update, including SQL

--- frontend/src/app/group/components/group-management.component.ts
+++ frontend/src/app/group/components/group-management.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
